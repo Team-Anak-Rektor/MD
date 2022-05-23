f519576406db55920c208530c0af825138b28658
v4lomyr <63693284+v4lomyr@users.noreply.github.com>
2022-05-23 17:21:10 +0700

    Setup Camera for Food Validator

--- app/src/main/java/com/bintang/bangkitcapstoneproject/MainActivity.kt
+++ app/src/main/java/com/bintang/bangkitcapstoneproject/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.bintang.bangkitcapstoneproject
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bintang.bangkitcapstoneproject.databinding.ActivityMainBinding
+import com.bintang.bangkitcapstoneproject.ui.ui_elements.FoodDetectorCamera
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // check permission
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSION,
+                REQUEST_CODE_PERMISSION
+            )
+        }
+
+        binding.cameraButton.setOnClickListener {
+            // open camera
+            val intent = Intent(this, FoodDetectorCamera::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSION = 10
     }
 }
